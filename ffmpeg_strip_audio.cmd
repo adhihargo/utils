589c515cb44fb196cca11c143e5d60753ba57d1f
Adhi Hargo <cadmus.sw@gmail.com>
2021-07-22 10:15:57 +0700

    Change variable expansion in existing batch files

--- ffmpeg_strip_audio.cmd
+++ ffmpeg_strip_audio.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 @SETLOCAL
 
-ffmpeg -i %1 -map 0:0 -codec copy "%~n1-%~x1"
+ffmpeg -i "%~1" -map 0:0 -codec copy "%~n1-%~x1"
 nircmd clonefiletime %1 "%~n1-%~x1"
 
 @ENDLOCAL
--- ffmpeg_strip_video.cmd
+++ ffmpeg_strip_video.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 @SETLOCAL
 
-ffmpeg -i %1 -map 0:1 -codec copy "%~n1_audio%~x1"
+ffmpeg -i "%~1" -map 0:1 -codec copy "%~n1_audio%~x1"
 nircmd clonefiletime %1 "%~n1_audio%~x1"
 
 @ENDLOCAL
