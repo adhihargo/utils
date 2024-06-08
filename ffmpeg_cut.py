fa3b42305ddd15d429d2c15e8836deaac056c368
Adhi Hargo <cadmus.sw@gmail.com>
2024-06-08 17:11:51 +0700

    ffmpeg_cut: Use configuration file

--- ffmpeg_cut.py
+++ ffmpeg_cut.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import configparser
 import logging
 import os
 import sys
@@ -20,6 +21,9 @@ def get_parser():
                         help="Timestamp of end point to cut to.")
     parser.add_argument("--suffix", dest="suffix", default="_",
                         help="Suffix to be appended to resulting filename.")
+    parser.add_argument("-c", "--conf", action="store_true",
+                        help="Mark input FILE as config file. See README for format. "
+                             "If set, other switches but -y and -V are ignored.")
     parser.add_argument("-y", dest="suppressQuestion", action="store_true",
                         help="Suppress question if file exists.")
     parser.add_argument("-V", dest="verbose", action="store_true",
@@ -31,6 +35,39 @@ def get_parser():
 def main():
     parser = get_parser()
     args = parser.parse_args()
+
+    if args.conf:
+        confFilePath = args.file_name
+        config = configparser.ConfigParser(interpolation=None)
+        config.read(confFilePath)
+        if not {"main", "sections"} <= set(config.sections()):
+            parser.error("config file does not contain the required sections.")
+        if not "src" in config["main"]:
+            parser.error("config file does not state source file.")
+
+        srcFilePath = config["main"]["src"]
+        if not os.path.isfile(srcFilePath):
+            parser.error("source file does not exist.")
+
+        # whitespace-separated sequence of section names to process
+        sectionStr = config["main"].get("sections", "")
+        sectionList = sectionStr.split(" ") if sectionStr else []
+
+        sectionPairs = config.items("sections")
+        for index, (section, value) in enumerate(sectionPairs):
+            # skip sections not listed in config["main"]["sections"]
+            if sectionList and section not in sectionList:
+                continue
+
+            sectionStart = value or None
+            sectionEnd = sectionPairs[index + 1][1] if (index < len(sectionPairs) - 1) else None
+            sectionSuffix = "_{:>02}".format(section)
+            dstFilePath = get_dstFileName(srcFilePath, sectionSuffix)
+
+            logger.info("Processing section {}: {} - {}".format(section, sectionStart, sectionEnd))
+            ffmpeg_commands.ffmpeg_cut(srcFilePath, dstFilePath, sectionStart, sectionEnd,
+                                       suppressQuestion=args.suppressQuestion, verbose=args.verbose)
+        return
 
     srcFilePath = args.file_name
     dstFilePath = get_dstFileName(srcFilePath, args.suffix)
