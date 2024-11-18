d4ba1b8ef4fb4eef1a6700ec5d71dcc18b4d5f1f
Adhi Hargo <cadmus.sw@gmail.com>
2024-11-18 11:51:41 +0700

    ffmpeg_strip_*: scripts accept multiple file arguments

--- ffmpeg_strip_audio.cmd
+++ ffmpeg_strip_audio.cmd
@@ -1,7 +1,12 @@
 @ECHO OFF
 @SETLOCAL
 
-ffmpeg -i "%~1" -map 0:0 -codec copy "%~n1-%~x1"
-nircmd clonefiletime %1 "%~n1-%~x1"
+:loop
+IF NOT 'x%1'=='x' (
+	ffmpeg -i "%~1" -map 0:0 -codec copy "%~n1-%~x1"
+	nircmd clonefiletime %1 "%~n1-%~x1"
+	SHIFT
+	GOTO :loop
+)
 
 @ENDLOCAL
--- ffmpeg_strip_video.cmd
+++ ffmpeg_strip_video.cmd
@@ -1,7 +1,12 @@
 @ECHO OFF
 @SETLOCAL
 
-ffmpeg -i "%~1" -map 0:1 -codec copy "%~n1_audio%~x1"
-nircmd clonefiletime %1 "%~n1_audio%~x1"
+:loop
+IF NOT 'x%1'=='x' (
+	ffmpeg -i "%~1" -map 0:1 -codec copy -f adts "%~n1.aac"
+	nircmd clonefiletime "%~1" "%~n1.aac"
+	SHIFT
+	GOTO :loop
+)
 
 @ENDLOCAL
