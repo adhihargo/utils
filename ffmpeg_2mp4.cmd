b5173575d55d51c725d964c08014598ce391a1fa
Adhi Hargo <cadmus.sw@gmail.com>
2026-06-02 05:53:24 +0700

    ffmpeg_2mp4: Delete original on successful conversion

--- ffmpeg_2mp4.cmd
+++ ffmpeg_2mp4.cmd
@@ -3,7 +3,11 @@
 
 :loop
 IF NOT 'x%1'=='x' (
-	ffmpeg -i "%~1" -codec copy "%~dpn1.mp4"
+	rem ffmpeg -hide_banner -fflags +discardcorrupt -i "%~1" -codec copy "%~dpn1.mp4"
+	ffmpeg -hide_banner -analyzeduration 100M -probesize 100M -i "%~1" -codec copy "%~dpn1.mp4"
+	if ERRORLEVEL 1 GOTO skipdel
+	del "%~1"
+:skipdel
 	SHIFT
 	GOTO :loop
 )
