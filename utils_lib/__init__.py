45201d30581c35e3f25c2d0451cfa9de8fec0bb0
Adhi Hargo <cadmus.sw@gmail.com>
2026-06-02 05:52:21 +0700

    utils_lib: Accomodate custom output directory

--- utils_lib/__init__.py
+++ utils_lib/__init__.py
@@ -1,9 +1,14 @@
 import os
 
 
-def get_dstFileName(srcFileName, suffix="_"):
+def get_dstFileName(srcFileName, suffix="_", dstDir=None):
     if not suffix:
         raise Exception("Suffix must not be empty.")
+
+    if dstDir is not None:
+        if not os.path.exists(dstDir):
+            os.makedirs(dstDir)
+        srcFileName = os.path.join(dstDir, os.path.basename(srcFileName))
 
     baseFileName, baseFileExt = os.path.splitext(srcFileName)
     dstFileName = "{}{}{}".format(baseFileName, suffix, baseFileExt)
