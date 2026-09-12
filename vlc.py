d395e1d49e0d117a74a3375d6536c891d5a2e3e3
Adhi Hargo <cadmus.sw@gmail.com>
2026-09-12 17:12:18 +0700

    vlc: - Add shortcuts to preset port numbers
    - Remove space around delimiters when writing config file
    - Configure logger

--- vlc.py
+++ vlc.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(os.path.splitext(os.path.basename(__file__))[0])
 
 
 class ConfigWindow(tk.Tk):
-    def __init__(self, port, geometry=None):
+    def __init__(self, args, geometry=None):
         super().__init__()
         self.resizable(True, False)
         if geometry is not None:
@@ -22,11 +22,12 @@ class ConfigWindow(tk.Tk):
         self.attributes("-topmost", True)
         self.title("VLC Control")
 
-        self.default_port = port
+        self.port_default = args.port
+        self.port_options = list(set(args.port_opts))
         self.accepted = False
         self.location_str = None
 
-        self.v_port = tk.IntVar(value=port)
+        self.v_port = tk.IntVar(value=args.port)
         self.spb_port = tk.Spinbox(
             master=self, from_=1024, to=65535, width=20, justify=tk.CENTER, textvariable=self.v_port,
             validate="key", validatecommand=(self.register(lambda s: s == "" or s.isdigit()), "%P"))
@@ -56,7 +57,7 @@ class ConfigWriter(object):
 
     def save(self):
         with open(CONFIG_PATH, "w") as config_file:
-            self.config.write(config_file)
+            self.config.write(config_file, space_around_delimiters=False)
 
 
 def get_config():
@@ -83,8 +84,13 @@ def get_parser_base(config):
     env_port = int(os.getenv("VLC_TARGET_PORT", 4212))
     if config:
         config_port = int(config["main"].get("port", env_port))
+        _port_opts_str = config["main"].get("opts", None)
+        _port_opts = (n.strip() for n in _port_opts_str.split(",")) if _port_opts_str else []
+        config_port_opts = [int(n) for n in _port_opts if n.isdigit()]
     else:
         config_port = env_port
+        config_port_opts = None
+    parser.add_argument("--port_opts", action="append", type=int, default=config_port_opts)
     parser.add_argument("-p", "--port", type=int, default=config_port)
     return parser
 
@@ -101,7 +107,13 @@ def vlc_send_cmd(address, cmd_str):
 
 def handle_spb_port_keys(window: ConfigWindow, event: tk.Event):
     if event.keysym == "r" and event.state & 0x4:
-        window.v_port.set(window.default_port)
+        window.v_port.set(window.port_default)
+    elif event.keysym.isdigit() and event.state & 0x4:
+        idx = int(event.keysym)
+        port_count = len(window.port_options)
+        if idx in range(1, port_count + 1):
+            port = window.port_options[idx - 1]
+            window.v_port.set(port)
     elif event.keysym == "Escape":
         window.quit()
     elif event.keysym == "Return":
@@ -119,7 +131,7 @@ def main():
         config_writer = ConfigWriter(config)
         location_str = config["main"].get("geometry")
 
-        window = ConfigWindow(port=args.port, geometry=location_str)
+        window = ConfigWindow(args=args, geometry=location_str)
         window.set_callback({"spb_port.<Key>": partial(handle_spb_port_keys, window)})
         window.mainloop()
 
@@ -141,6 +153,7 @@ def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     try:
         main()
     except Exception:
